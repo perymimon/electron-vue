afb5c2bb52ec559ca9862eb57bd28a75d96367ef
perymimon <pery.mimon@gmail.com>
2018-12-14 19:27:42 +0200

    auto load global components

--- src/main.js
+++ src/main.js
@@ -4,6 +4,27 @@ import store from './store'
 import AsyncComputed from 'vue-async-computed'
 import FileList from './globalComponents/FileList'
 
+// param 0: The relative path of the components folder
+// param 1: Whether or not to look in subfolders
+// param 2: The regular expression used to match base component filenames
+const requireComponent = require.context('./globalComponents', false, /[A-Z]\w+\.vue$/ );
+
+// Get component config
+requireComponent.keys().forEach(fileName => {
+
+    const componentConfig = requireComponent(fileName);
+
+    // Get PascalCase name of component
+    // Strip the leading `./` and extension from the filename
+    const componentName = fileName.replace(/^\.\/(.*)\.\w+$/, '$1');
+
+    // Register component globally
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    Vue.component(componentName, componentConfig.default || componentConfig )
+});
+
 import editor from 'vue2-medium-editor'
 import 'medium-editor/dist/css/medium-editor.css'
 import 'medium-editor/dist/css/themes/default.css'
@@ -11,7 +32,7 @@ import 'medium-editor/dist/css/themes/default.css'
 Vue.config.productionTip = false;
 Vue.use(AsyncComputed);
 
-Vue.component('FileList', FileList);
+// Vue.component('FileList', FileList);
 Vue.component('medium-editor', editor);
 
 new Vue({
